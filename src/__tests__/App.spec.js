03db489cd0935a6dc716129e3ed9f17bf98bc937
GomezOrtiz <david.gomez.mail@gmail.com>
2020-07-15 10:32:40 +0200

    Testing protected routes in app

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,21 +1,93 @@
 import App from "@/App"
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import VueRouter from "vue-router"
+import router, { HOME_PATH, TASKS_PATH, TASK_ADD_PATH, TASK_EDIT_PATH, LOGIN_PATH, SIGNUP_PATH } from "@/router"
+import Vuex from "vuex"
+import store from "@/store"
+import Vuelidate, { validationMixin } from 'vuelidate'
+import Home from "@/views/home/Home"
+import Tasks from "@/views/tasks/Tasks"
+import TaskAdd from "@/views/tasks/TaskAdd"
+import TaskEdit from "@/views/tasks/TaskEdit"
+import Login from "@/views/auth/Login"
+import Signup from "@/views/auth/Signup"
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
+localVue.use(Vuex)
+localVue.use(Vuelidate)
+
+const MOCK_USER = {uid: 98765, email: ""}
+store.dispatch("setLoggedInUser", MOCK_USER)
+
+jest.mock("@/firebase")
+import { auth } from "@/firebase"
 
 describe("App", () => {
 
     let wrapper
 
-    beforeEach(() => {
-        wrapper = shallowMount(App, {
-            localVue
+    beforeEach(async () => {
+        wrapper = mount(App, {
+            localVue,
+            router,
+            store
         })
+        auth.currentUser = MOCK_USER
     })
 
-    it("should render", () => {
-        expect(wrapper.exists()).toBe(true)
+    it("should render Home", async () => {
+        expect(wrapper.findComponent(Home).exists()).toBe(true)
+    })
+
+    it("should redirect to Login when going to Tasks if there's not a current user", async () => {
+
+        auth.currentUser = null
+
+        try {
+            await router.push(TASKS_PATH)
+        } catch(error) {
+            expect(error.message).toEqual(`Redirected when going from "${HOME_PATH}" to "${TASKS_PATH}" via a navigation guard.`)
+        }
+    })
+
+    it("should render Tasks in /tasks", async () => {
+
+        await router.push(TASKS_PATH)
+
+        expect(router.currentRoute.path).toEqual(TASKS_PATH)
+        expect(wrapper.findComponent(Tasks).exists()).toBe(true)
+    })
+
+    it("should render TaskAdd in /tasks/add", async () => {
+
+        await router.push(TASK_ADD_PATH)
+
+        expect(router.currentRoute.path).toEqual(TASK_ADD_PATH)
+        expect(wrapper.findComponent(TaskAdd).exists()).toBe(true)
+    })
+
+    it("should render TaskEdit in /tasks/edit", async () => {
+
+        await router.push(`${TASK_EDIT_PATH}/12345`)
+
+        expect(router.currentRoute.path).toEqual(`${TASK_EDIT_PATH}/12345`)
+        expect(wrapper.findComponent(TaskEdit).exists()).toBe(true)
+    })
+
+    it("should render Login in /login", async () => {
+
+        await router.push(LOGIN_PATH)
+
+        expect(router.currentRoute.path).toEqual(LOGIN_PATH)
+        expect(wrapper.findComponent(Login).exists()).toBe(true)
+    })
+
+    it("should render Signup in /signup", async () => {
+
+        await router.push(SIGNUP_PATH)
+
+        expect(router.currentRoute.path).toEqual(SIGNUP_PATH)
+        expect(wrapper.findComponent(Signup).exists()).toBe(true)
     })
 })
